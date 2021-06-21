e96a4119016f0db592400c79dc1ddce5032cf1b7
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2021-06-21 13:07:24 +0200

    abc

---
+++ windows-forms/abc/MainForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: walzju
+ * Date: 15.06.2021
+ * Time: 16:57
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace abc
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Wingdings", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+			this.button1.Location = new System.Drawing.Point(108, 85);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(127, 125);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Checked = true;
+			this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox1.Font = new System.Drawing.Font("Wingdings", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+			this.checkBox1.Location = new System.Drawing.Point(108, 55);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(104, 24);
+			this.checkBox1.TabIndex = 1;
+			this.checkBox1.Text = "checkBox1";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1CheckedChanged);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(241, 59);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(203, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "HHHHHHUUUUUUUUIIIIIIIIIIIIII";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Wingdings", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+			this.label2.Location = new System.Drawing.Point(188, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(464, 61);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "BOAH FETTES PROGRAM";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(2, 221);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(696, 65);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "HELO, YOUR COMPUTER HAS VIRUS";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label3.Click += new System.EventHandler(this.Label3Click);
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Wingdings", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+			this.label4.Location = new System.Drawing.Point(451, 141);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(99, 48);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "FE";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label4.Click += new System.EventHandler(this.Label4Click);
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(292, 142);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(194, 52);
+			this.label5.TabIndex = 6;
+			this.label5.Text = "Is for me?";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(292, 98);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(127, 23);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Virus löschen";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			this.button2.MouseEnter += new System.EventHandler(this.Button2MouseEnter);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(686, 299);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "abc";
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ windows-forms/abc/MainForm.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: walzju
+ * Date: 15.06.2021
+ * Time: 16:57
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace abc
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void CheckBox1CheckedChanged(object sender, EventArgs e)
+		{
+			if (checkBox1.Checked) button1.Enabled = true;
+			else button1.Enabled = false;
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			button1.Text = "BJUGVUIPGVUIVFZIVZI";
+			if (label3.ForeColor == Color.Yellow)
+				label3.ForeColor = Color.Black;
+			else
+				label3.ForeColor = Color.Yellow;
+				
+		}
+		
+		void Label3Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		
+		
+		void Label4Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void Button2MouseEnter(object sender, EventArgs e)
+		{
+			  Random random= new Random();
+			  button2.Location = new Point(random.Next(0, this.Width - button2.Size.Width), random.Next(0, this.Height - 4*button2.Size.Height));
+			  //
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
