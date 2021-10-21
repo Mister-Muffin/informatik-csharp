1d9183ca0b0f78b9fb4aad1c7c3329f975bfa72b
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2021-10-21 13:16:15 +0200

    added ulam project

---
+++ windows-forms/ulam/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 21.10.2021
+ * Time: 12:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace ulam
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textFieldN = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 97);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(260, 23);
+			this.label1.TabIndex = 0;
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// textFieldN
+			// 
+			this.textFieldN.Location = new System.Drawing.Point(94, 44);
+			this.textFieldN.Name = "textFieldN";
+			this.textFieldN.Size = new System.Drawing.Size(100, 20);
+			this.textFieldN.TabIndex = 1;
+			this.textFieldN.TextChanged += new System.EventHandler(this.TextFieldNTextChanged);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.textFieldN);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "ulam";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.TextBox textFieldN;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ windows-forms/ulam/MainForm.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 21.10.2021
+ * Time: 12:17
+ *
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ulam {
+    public partial class MainForm : Form {
+
+        int counter = 0;
+
+        public MainForm() { InitializeComponent(); }
+
+        private int ulam(int n) {
+            if (n == 1) {
+                label1.Text = "Bei " + n.ToString() + " angekommen. counter: " + counter.ToString();
+                return n;
+            }
+
+            counter++; //
+
+            if (n > 1 && n % 2 == 0) {
+                return n = ulam(n / 2);
+            }
+            if (n > 1 && n % 2 != 0) {
+                return n = ulam(3 * n + 1);
+            }
+            label1.Text = n.ToString() + "counter: " + counter.ToString();
+            return -1;
+        }
+
+        void TextFieldNTextChanged(object sender, EventArgs e) {
+            textFieldN.Text = a(textFieldN.Text);
+            //
+            counter = 0;
+            try {
+                ulam(Convert.ToInt32(textFieldN.Text));
+            } catch (System.Exception) {
+                label1.Text = "Exception";
+            }
+        }
+        //---------------------------------------------------------
+        private string a(string text) {
+            try {
+                if (text.Contains("+")) {
+                    text = text.Remove(text.IndexOf("+"), 1);
+                    text = (Convert.ToInt32(text) + 1).ToString();
+                    return text;
+                }
+                if (text.Contains("-")) {
+                    text = text.Remove(text.IndexOf("-"), 1);
+                    text = (Convert.ToInt32(text) - 1).ToString();
+                    return text;
+                }
+                return text;
+            } catch (Exception) { return text; }
+        }
+    }
+}
