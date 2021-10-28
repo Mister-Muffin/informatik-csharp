fc08910c589b53d948a2f856f1404b78897ec8e5
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2021-10-28 18:59:11 +0200

    added keineAhnung

---
+++ windows-forms/keineAhnung/MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 28.10.2021
+ * Time: 18:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace keineAhnung
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 39);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(260, 20);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(260, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Bidde gib mehrere einzelne Wörter ein";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 65);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(260, 184);
+			this.dataGridView1.TabIndex = 2;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "keineAhnung";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
---
+++ windows-forms/keineAhnung/MainForm.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 28.10.2021
+ * Time: 18:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data;
+
+namespace keineAhnung {
+    /// <summary>
+    /// Description of MainForm.
+    /// </summary>
+    public partial class MainForm : Form {
+        DataTable table = new DataTable();
+        public MainForm() {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+
+            table.Columns.Add("1", typeof(String));
+            table.Columns.Add("2", typeof(String));
+
+            //
+            // TODO: Add constructor code after the InitializeComponent() call.
+            //
+        }
+
+        void TextBox1TextChanged(object sender, EventArgs e) {
+            try {
+                var input = textBox1.Text.ToString();
+                List<String> list = new List<String>();
+
+                string[] subs = input.Split(' ');
+                table.Rows.Clear();
+
+                foreach (var sub in subs) {
+                    list.Add(sub);
+                    table.Rows.Add(sub, "");
+                }
+
+                dataGridView1.DataSource = table;
+            } catch (Exception) { }
+        }
+    }
+}
